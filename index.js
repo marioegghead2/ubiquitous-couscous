58008fded4b2fdc8f5dafac9ca91711a70e5a7dd
Abel Sengkey <82485570+marioegghead2@users.noreply.github.com>
2021-08-28 12:13:52 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+Vue.component('button-counter', {
+  data: function () {
+    let obj = {
+      count: 0
+    }
+    return obj
+  },
+  template: '<button v-on:click="count++">{{ count }} 👏 claps</button>'
+})
+
+let app = new Vue ({
+  el: '#app',
+  data: {
+    myData: 'Super cool VueJS application 🖖'
+  }
+})
